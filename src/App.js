154193578935622a93a47a58e3c4e48a5d5d5f1f
fathimamohamed <fathimamohamed2510@gmail.com>
2023-09-22 01:55:58 +0530

    Removing alert.js fileand its import in App.js file

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Routing from './components/Routing';
-import { AlertProvider } from "./components/context/alertContext";
-import Alert from "./components/Alert";
 
 function App() {
   return (
     <>
-    <AlertProvider>
       <Navbar />
       <Routing />
       <Footer />
-      <Alert />
-    </AlertProvider>
     </>
   );
 }
